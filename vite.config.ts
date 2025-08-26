be6fdf27000d9a5f968413ffd61e146899f1368e
Aditya Negi <adi@Adis-MacBook-Pro.local>
2025-08-26 12:08:51 +0530

    setup github actions deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/osiris-marketing/',  // 👈 must match your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
